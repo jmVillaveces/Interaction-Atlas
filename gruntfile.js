bf044c4e078351201405b851d0a4543a9d3c9e64
jVillaveces <secevalliv@gmail.com>
2014-09-19 16:52:38 +0200

    watch

--- gruntfile.js
+++ gruntfile.js
@@ -1,8 +1,17 @@
 module.exports = function(grunt) {
     
+    //Load Tasks
+    grunt.loadTasks('tasks');
+    
     // Project configuration.
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        watch:{
+            precompile:{
+                files: ['templates/*.hbs'],
+                tasks: ['precompile']
+            }
+        },
         jshint: {
             files: ['gruntfile.js', 'tasks/*.js'],
             options: {
@@ -17,9 +26,11 @@ module.exports = function(grunt) {
         }
     });
 
+    
+    
     // Load the plugins
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
     
-    //Load Tasks
-    grunt.loadTasks('tasks');
+    
 };
