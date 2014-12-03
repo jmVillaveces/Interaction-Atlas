e6b0948c1cd62dbd3d4435ab53a18b0e659012cb
jVillaveces <secevalliv@gmail.com>
2014-12-03 15:35:21 +0100

    adition of progress bar and message

--- js/views/home.js
+++ js/views/home.js
@@ -1,10 +1,12 @@
 var templates = require('../templates');
-var $ = jQuery;
+var Progressbar = require('./progressbar');
 
+var progressBar = null;
+var clazz = 'alert-info';
 module.exports = Backbone.View.extend({
     
     initialize: function(options){
-       this.options = options;
+        this.options = options;
     },
     
     events:{
@@ -17,10 +19,18 @@ module.exports = Backbone.View.extend({
         var tpl = templates.home(this.options);
         $(this.options.el).html(tpl);
         
-        this.query = $("#query");
+        this.query = $('#query');
         
         //Set focus on query
         this.query.focus();
+        
+        //Hide msg
+        $('#msg').hide();
+        
+        //Init progress bar hidden
+        $('#loading').hide();
+        progressBar = new Progressbar({el:'#loading'});
+        progressBar.render();
     },
     
     example : function(){
@@ -36,7 +46,28 @@ module.exports = Backbone.View.extend({
     search : function(e){
         e.preventDefault();
         
+        $('#loading').show();
+        $('#search').hide();
+        
         // trigger search event
         iAtlas.vent.trigger('search', this.query.val(), $('#expand').prop("checked"));
+    },
+    
+    update : function(n){
+        progressBar.update(n);
+    },
+    
+    alert : function(msg, type){
+        var el = $('#msg').html(msg).show();
+        
+        if(type && type !== clazz){
+            el.toggleClass(clazz +' '+ type);
+            clazz = type;
+        }
+    },
+    
+    showSearch : function(){
+        $('#loading').hide();
+        $('#search').show();
     }
 });
