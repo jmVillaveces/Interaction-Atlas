3565f01276760819ec3dd692144d9d141f486108
jVillaveces <secevalliv@gmail.com>
2014-12-16 15:47:50 +0100

    save button imlemented

--- js/views/network.js
+++ js/views/network.js
@@ -9,7 +9,8 @@ module.exports = Backbone.View.extend({
     },
 
     events:{
-        'keyup #search': 'keyAction'
+        'keyup #search': 'keyAction',
+        'click #save' : 'save'
     },
     
     render: function(){
@@ -21,5 +22,18 @@ module.exports = Backbone.View.extend({
     keyAction: function(e){
         var value = $('#search').val();
         this.network.search(value);
+    },
+    
+    save : function(){
+        
+        var svg = d3.selectAll('#network');
+        var html ='<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="'+svg.attr('width')+'" height="'+svg.attr('height')+'">' + 
+        svg.node().innerHTML +'</svg>';
+                
+            
+        var l = document.createElement('a');
+            l.download = 'sample.svg';
+            l.href = 'data:image/svg+xml;base64,' + btoa(html);
+            l.click();
     }
 });
