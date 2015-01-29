f96cdf088764cde4211695ca5cd33f5ff9be8aa7
jVillaveces <secevalliv@gmail.com>
2015-01-29 13:50:36 +0100

    visualization mocule added

--- js/views/network.js
+++ js/views/network.js
@@ -1,5 +1,5 @@
 var templates = require('../templates');
-var viz = require('../lib/network.viz.js');
+var vis = require('../vis/visualization');
 
 module.exports = Backbone.View.extend({
     
@@ -17,7 +17,8 @@ module.exports = Backbone.View.extend({
     render: function(){
         var tpl = templates.network();
         $(this.options.el).html(tpl);
-        this.network = viz.selector('#network').data(this.data).width($(window).width()).height($(window).height()).update();
+        this.network = vis.data(this.data).selector('#network').update();
+        //viz.selector('#network').data(this.data).width($(window).width()).height($(window).height()).update();
     },
     
     keyAction: function(e){
@@ -30,15 +31,18 @@ module.exports = Backbone.View.extend({
         var svg = d3.selectAll('#network');
         var html ='<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="'+svg.attr('width')+'" height="'+svg.attr('height')+'">' + 
         svg.node().innerHTML +'</svg>';
-                
             
         var l = document.createElement('a');
-            l.download = 'sample.svg';
+            l.download = 'network.svg';
             l.href = 'data:image/svg+xml;base64,' + btoa(html);
             l.click();
     },
     
     pathway : function(){
         Backbone.trigger('pathway');
+    },
+    
+    setLayout : function(l){
+        this.network.layout(l).update();
     }
 });
