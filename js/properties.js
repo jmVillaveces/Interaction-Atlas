fa288b4816d00b23c5d7d1a053fda9aa8222c1ba
jVillaveces <secevalliv@gmail.com>
2015-02-04 13:33:19 +0100

    new property to retireve protein ids from uniprot

--- js/properties.js
+++ js/properties.js
@@ -2,5 +2,6 @@ module.exports = {
     psicquicServer: //'http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/',
     'http://dachstein.biochem.mpg.de:8080/iatlas/webservices/current/search/',
     proxy : '../proxy/proxy.php',
-    example : 'P49959,P25454,Q54KD8,O74773,Q8IV36,Q96B01,Q54CS9,P52701,Q9CXE6,Q7T6Y0,Q682D3'
+    example : 'P49959,P25454,Q54KD8,O74773,Q8IV36,Q96B01,Q54CS9,P52701,Q9CXE6,Q7T6Y0,Q682D3',
+    autocomplete : 'http://www.uniprot.org/uniprot/?format=tab&columns=id&limit=5&query=id:'
 };
