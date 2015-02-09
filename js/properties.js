f5cbbb90d68c5320a335f4e772fdfa04d64161fc
jVillaveces <secevalliv@gmail.com>
2015-02-09 15:34:17 +0100

    changed proxy to heroku

--- js/properties.js
+++ js/properties.js
@@ -1,7 +1,7 @@
 module.exports = {
     psicquicServer: //'http://www.ebi.ac.uk/Tools/webservices/psicquic/intact/webservices/current/search/',
     'http://dachstein.biochem.mpg.de:8080/iatlas/webservices/current/search/',
-    proxy : '../proxy/proxy.php',
+    proxy : 'https://cors-anywhere.herokuapp.com/',
     example : 'P49959,P25454,Q54KD8,O74773,Q8IV36,Q96B01,Q54CS9,P52701,Q9CXE6,Q7T6Y0,Q682D3',
     autocomplete : 'http://www.uniprot.org/uniprot/?format=tab&columns=id&limit=5&query=id:'
 };
