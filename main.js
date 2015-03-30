a77e23e25db377489189c8a77db439173c9d61d5
jVillaveces <secevalliv@gmail.com>
2015-03-30 17:22:27 +0200

    main app file

--- main.js
+++ main.js
@@ -5,6 +5,62 @@ Backbone.$ = jQuery;
 Handlebars = require('handlebars');
 _ = require('underscore');
 
+require('./js/helpers.js'); // Handelbars helpers
+require('bootstrap/dist/js/bootstrap.min.js'); // bootstrap
+
+App = {};
+
+//Model
+var Model = require('./js/models/main.js');
+App.model = new Model();
+
+//Views
+var Main = require('./js/views/main');
+var Navbar = require('./js/views/navbar');
+var SideMenu = require('./js/views/sidemenu');
+var Logger = require('./js/views/logger');
+var Graph = require('./js/views/graph');
+var Options = require('./js/views/options');
+var UPDialog = require('./js/views/UPdialog');
+
+App.views = {};
+
+//Main
+App.views.main = new Main({el:'body'});
+App.views.main.render();
+
+//Nav bar
+App.views.navbar = new Navbar({el:'#navbar'});
+App.views.navbar.render();
+
+App.views.side = new SideMenu({el:'#sidemenu'});
+App.views.side.render();
+
+App.views.options = new Options({el:'#profile'});
+App.views.options.render();
+
+//Dialog
+App.views.UPDialog = new UPDialog({el:'#dialog'});
+
+//Logger
+App.views.logger = new Logger({el:'#logger'});
+
+//Graph
+App.views.graph = new Graph({el:'network'});
+
+//Router
+var Router = require('./js/router/router');
+App.router = new Router();
+Backbone.history.start();
+
+
+/*jQuery = $ = require('jquery');
+Backbone = require('backbone');
+Backbone.$ = jQuery;
+
+Handlebars = require('handlebars');
+_ = require('underscore');
+
 var Atlas = require('./js/models/atlas.js');
 var UPprotein = require('./js/models/UPprotein');
 
@@ -165,4 +221,4 @@ var _init = function(){
     var proxy = function(url){return iAtlas.properties.proxy+url;};
     psicquic.url(iAtlas.properties.psicquicServer).proxy(proxy).count('*', _homeView);
 }();
-
+*/
