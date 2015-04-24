046b487476d8da49e2156e8cf15977cc77834721
jVillaveces <secevalliv@gmail.com>
2015-04-24 14:41:10 +0200

    implemented discrete and direct mappings

--- js/views/attributeSetter.js
+++ js/views/attributeSetter.js
@@ -8,6 +8,8 @@ module.exports = Backbone.View.extend({
     
     initialize: function(options){
         this.options = options;
+        
+        this.events['click #' + _okBtnId ] = 'onOkButton';
     },
     
     events : {
@@ -21,6 +23,8 @@ module.exports = Backbone.View.extend({
         this.control.attributes = {};
         this.control.tag = render.control.prop('tagName');
         this.control.color = render.control.hasClass('minicolors-input');
+        
+        this.cssAttr = render.control.prop('name');
         
         this.elements = render.elements;
  
@@ -57,8 +61,12 @@ module.exports = Backbone.View.extend({
         $('#' + _dialId).modal('show');
         
         $('#' + _dialId + ' .modal-body').html(templates.attributeSetter({ 
-            properties : render.properties
+            properties : render.properties,
+            transform : (this.control.tag === 'INPUT'),
+            control : this.control
         }));
+        
+        if(this.control.color) $('.transform .minicolors-input').minicolors({ theme:'bootstrap'});
         
         this.renderContent(render.properties[0]);
     },
@@ -79,9 +87,36 @@ module.exports = Backbone.View.extend({
         if(this.control.color) $('#discrete .minicolors-input').minicolors({ theme:'bootstrap'});
     },
     
+    setTransform : function(mapping){
+        
+        if(mapping === 'transform'){
+            var attr = $('select[name=attrs]').val();
+            var min = this.elements.min(function(e){
+                return e.data(attr);
+            });
+
+            var max = this.elements.max(function(e){
+                return e.data(attr);
+            });
+            
+            if(_.isNumber(min.value) && _.isFinite(min.value) && _.isNumber(max.value) && _.isFinite(max.value)){
+                $('.log').hide();
+                $('.transform').show();
+            }else{
+                $('.log').show();
+                $('.transform').hide();
+            }
+            
+        }else{
+            $('.log').hide();
+            $('.transform').hide();
+        }
+    },
+    
     onMappingChange : function(e){
         var mapping = $(e.target).val();
         
+        this.setTransform(mapping);
         if(mapping === 'direct'){
             $('#discrete').hide();
         }else if (mapping === 'transform'){
@@ -93,5 +128,28 @@ module.exports = Backbone.View.extend({
     
     onAttrsChange : function(e){
         this.renderContent($(e.target).val());
+        this.setTransform($('select[name=mappings]').val());
+    },
+    
+    onOkButton : function(e){
+        var attr = $('select[name=attrs]').val(), mapping = $('select[name=mappings]').val();
+        
+        var elements = this.elements, cssAttr = this.cssAttr;
+        if(mapping === 'direct'){
+            App.views.graph.cy.batch(function(){
+                elements.filter('[' + attr + ']').forEach(function(ele){
+                    ele.css(cssAttr, ele.data(attr));
+                });
+            });
+        }else if(mapping === 'discrete'){
+            App.views.graph.cy.batch(function(){
+                _.each($('#discrete input'), function(i){
+                    //$(i).val();
+                     elements.filter('[' + attr + '="' + $(i).prop('name') + '"]').forEach(function(ele){
+                        ele.css(cssAttr, $(i).val());
+                    });
+                });
+            });
+        }
     }
 });
