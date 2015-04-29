c128b524e68bb34c50d09a7887fc9d363fceb376
jVillaveces <secevalliv@gmail.com>
2015-04-29 15:54:21 +0200

    implemented search by attribute

--- js/views/navbar.js
+++ js/views/navbar.js
@@ -1,6 +1,6 @@
 var templates = require('../templates');
 
-var _pathwayId = _.uniqueId('pathway_'), _settingsId = _.uniqueId('settings_'), _findId = _.uniqueId('find_'), _saveId = _.uniqueId('save_'), _importId = _.uniqueId('import_'), _layoutId = _.uniqueId('layout_');
+var _pathwayId = _.uniqueId('pathway_'), _settingsId = _.uniqueId('settings_'), _findId = _.uniqueId('find_'), _saveId = _.uniqueId('save_'), _importId = _.uniqueId('import_'), _layoutId = _.uniqueId('layout_'), _attrListId = _.uniqueId('attrList_');
 
 module.exports = Backbone.View.extend({
     
@@ -13,9 +13,11 @@ module.exports = Backbone.View.extend({
         this.events['click #' + _saveId] = 'onSave';
         this.events['click #' + _importId] = 'onImport';
         this.events['click #' + _layoutId + ' a'] = 'onLayout';
+        this.events['click #' + _attrListId + ' a'] = 'onSearchAttrClick';
         
         // Update when graph is ready
         this.listenTo(Backbone, 'layout_changed', this.onLayoutChanged);
+        this.listenTo(Backbone, 'got_data', this.onGotData);
     },
     
     events: {
@@ -30,8 +32,8 @@ module.exports = Backbone.View.extend({
             settingsId : _settingsId,
             findId : _findId,
             saveId : _saveId,
-            layoutId:_layoutId
-        
+            layoutId : _layoutId,
+            attrListId : _attrListId
         });
         $(this.options.el).append(tpl);
     },
@@ -59,7 +61,17 @@ module.exports = Backbone.View.extend({
     
     onFind : function(e){
         var searchTerm = $('#' + _findId).val();
-        App.views.graph.find(searchTerm);
+        var attr = $('#' + _attrListId + ' .glyphicon-ok').parent().text();
+        
+        var cy = App.views.graph.cy;
+        if(cy){
+            cy.elements().unselect();
+            
+            if(searchTerm.length){
+                searchTerm = (_.isNaN(+searchTerm)) ? '"' + searchTerm + '"' : +searchTerm;
+                cy.elements('[' + attr + ' *= ' + searchTerm + ']').select();
+            }
+        }      
     },
     
     onImport: function(e){
@@ -82,7 +94,6 @@ module.exports = Backbone.View.extend({
     },
     
     onLayoutChanged : function(l){
-        console.log(l);
         $('#' + _layoutId + ' a').each(function(){
             var link = $(this);
             var layout = link.text().toLowerCase().trim();
@@ -98,5 +109,22 @@ module.exports = Backbone.View.extend({
     onMoveChange:function(e){
         var val = ($(e.target).val() === 'select') ? true : false;
         App.views.graph.boxSelectionEnabled(val);
+    },
+    
+    onSearchAttrClick : function(e){
+        $('#' + _attrListId + ' span').removeClass('glyphicon-ok');
+        $(e.target).children().addClass('glyphicon-ok');
+    },
+    
+    onGotData : function(e){
+        var attrs = [].concat(App.model.attributes.nodeAttributes, App.model.attributes.edgeAttributes);
+        attrs = _.uniq(attrs);
+        
+        var aStr = '';
+        _.each(attrs, function(a, i){
+            var cl = (i === 0) ? 'glyphicon-ok' : '';
+            aStr += '<li><a href="#">' + a + ' <span class="glyphicon ' + cl + '" aria-hidden="true"></span></a></li>';
+        });
+        $('#' + _attrListId).html(aStr);
     }
 });
