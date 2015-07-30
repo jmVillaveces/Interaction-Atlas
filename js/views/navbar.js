4548233f78945bb889a1cfa8b0e123397da43dde
jVillaveces <secevalliv@gmail.com>
2015-07-30 15:24:40 +0200

    handling community detection button events

--- js/views/navbar.js
+++ js/views/navbar.js
@@ -1,6 +1,6 @@
 var templates = require('../templates');
 
-var _pathwayId = _.uniqueId('pathway_'), _settingsId = _.uniqueId('settings_'), _findId = _.uniqueId('find_'), _saveId = _.uniqueId('save_'), _importId = _.uniqueId('import_'), _layoutId = _.uniqueId('layout_'), _attrListId = _.uniqueId('attrList_');
+var _pathwayId = _.uniqueId('pathway_'), _settingsId = _.uniqueId('settings_'), _findId = _.uniqueId('find_'), _saveId = _.uniqueId('save_'), _commId = _.uniqueId('comm_'), _importId = _.uniqueId('import_'), _layoutId = _.uniqueId('layout_'), _attrListId = _.uniqueId('attrList_'), _dialId = _.uniqueId('dial_'), _okBtnId = _.uniqueId('ok_'), _communities = {};
 
 module.exports = Backbone.View.extend({
     
@@ -11,9 +11,11 @@ module.exports = Backbone.View.extend({
         this.events['click #' + _settingsId ] = 'onSettingsClick';
         this.events['keyup #' + _findId ] = 'onFind';
         this.events['click #' + _saveId] = 'onSave';
+        this.events['click #' + _commId] = 'onCommDetection';
         this.events['click #' + _importId] = 'onImport';
         this.events['click #' + _layoutId + ' a'] = 'onLayout';
         this.events['click #' + _attrListId + ' a'] = 'onSearchAttrClick';
+        this.events['click #' + _okBtnId] = 'onOkButton';
         
         // Update when graph is ready
         this.listenTo(Backbone, 'layout_changed', this.onLayoutChanged);
@@ -33,7 +35,8 @@ module.exports = Backbone.View.extend({
             findId : _findId,
             saveId : _saveId,
             layoutId : _layoutId,
-            attrListId : _attrListId
+            attrListId : _attrListId,
+            commId : _commId
         });
         $(this.options.el).append(tpl);
     },
@@ -125,5 +128,49 @@ module.exports = Backbone.View.extend({
             aStr += '<li><a href="#">' + a + ' <span class="glyphicon ' + cl + '" aria-hidden="true"></span></a></li>';
         });
         $('#' + _attrListId).html(aStr);
+    },
+    
+    onCommDetection : function(e){
+        
+        var nodes = App.model.attributes.interactors.pluck('id');
+        var edges = App.model.attributes.interactions.map(function(e){return {source: e.attributes.source, target: e.attributes.target};});
+        
+        if(nodes.length && edges.length){
+            
+            var community = jLouvain().nodes(nodes).edges(edges).partition_init();
+            _communities  = community();
+
+            App.views.graph.cy.nodes().each(function(i,e){
+                e.data().community = _communities[e.id()];
+            });
+            
+            var options = {
+                title : 'Community Detection', 
+                icon : 'glyphicon-th-large', 
+                okBtn : 'Ok', 
+                okBtnIcon : 'glyphicon-tint',
+                loading : 'Loading ...',
+                dialId : _dialId,
+                okBtnId : _okBtnId
+            };
+            
+            var tpl = templates.dialog(options);
+            $(this.options.el).append(tpl);
+            $('#' + _dialId).modal('show');
+            $('#' + _dialId + ' .modal-body').html( '<h3>' + _.uniq(_.values(_communities)).length + ' communities detected.<small> Would you like to <strong>colour</strong> the nodes acordingly? </small></h3>');
+        }
+    },
+    
+    onOkButton : function(e){
+        var cy =  App.views.graph.cy;
+        var i = 0;
+        var cols = ['#1f77b4','#aec7e8','#ff7f0e','#ffbb78','#2ca02c','#98df8a','#d62728','#ff9896','#9467bd','#c5b0d5','#8c564b','#c49c94','#e377c2','#f7b6d2','#7f7f7f','#c7c7c7','#bcbd22','#dbdb8d','#17becf','#9edae5'];
+        cy.startBatch();
+        _.each(_.uniq(_.values(_communities)), function(v){
+            cy.nodes('[community='+ v +']').css('background-color', cols[i]);
+            i = (i<cols.length) ? i + 1 : 0;
+        });
+        cy.endBatch();
+        $('#' + _dialId).modal('hide');
     }
 });
