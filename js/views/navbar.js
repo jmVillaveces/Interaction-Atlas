d7666b8fbb6b67be1805e0ef7fe0193ef3d2b011
jVillaveces <secevalliv@gmail.com>
2015-07-30 15:28:13 +0200

    using  d3.scale.category20c();

--- js/views/navbar.js
+++ js/views/navbar.js
@@ -164,7 +164,7 @@ module.exports = Backbone.View.extend({
     onOkButton : function(e){
         var cy =  App.views.graph.cy;
         var i = 0;
-        var cols = ['#1f77b4','#aec7e8','#ff7f0e','#ffbb78','#2ca02c','#98df8a','#d62728','#ff9896','#9467bd','#c5b0d5','#8c564b','#c49c94','#e377c2','#f7b6d2','#7f7f7f','#c7c7c7','#bcbd22','#dbdb8d','#17becf','#9edae5'];
+        var cols = d3.scale.category20c();
         cy.startBatch();
         _.each(_.uniq(_.values(_communities)), function(v){
             cy.nodes('[community='+ v +']').css('background-color', cols[i]);
